af0db3ca0aee54486bd9fea787975012388dc819
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-10.local>
2019-02-08 11:23:14 -0500

    Completed factorial

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,10 @@ import java.math.BigInteger;
 public class Factorial {
 
     public BigInteger factorialOf(Integer value){
-        return null;
+        BigInteger fact = new BigInteger("1");
+        for (Integer i = 1; i <= value; i++){
+        fact = fact.multiply(BigInteger.valueOf(i));
     }
-
+return fact;
+}
 }
