692f7b6dead8e0df83906450715616fe7a92b362
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-10.local>
2019-02-08 10:37:14 -0500

    Completed largest integer

--- LargestInteger.java
+++ LargestInteger.java
@@ -1,13 +1,29 @@
  
-
-
+import java.util.Scanner;
 public class LargestInteger {
 
     public Integer findLargestNumberUsingConditional(Integer[] integers){
+        int x = integers[0];
+        int y = integers[1];
+        int z = integers[2];
+        
+      if (x > y && x > z){
+        return x;
+    } else if (y > x && y > z){
+        return y;
+    } else if (z > x && z > y){
+        return z;  
+    } else {
         return null;
-    }
+
+} 
+
+}
 
     public Integer findLargestNumberUsingMathMax(Integer[] integers){
-        return null;
+      int x = integers[0];
+      int y = integers[1];
+      int z = integers[2];
+      return Math.max(Math.max(x,y),z);
     }
 }
