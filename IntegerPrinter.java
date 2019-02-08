12ec7da7a3838da0ee9e8f7468c20aba1951af02
Alexandra Bozzini <alexandrabozzini@zipcodersmbp10.lan>
2019-02-07 19:38:14 -0500

    Completed integer printer section

--- IntegerPrinter.java
+++ IntegerPrinter.java
@@ -1,18 +1,18 @@
  
 
-
+import java.util.Scanner;
 public class IntegerPrinter {
 
     public String printIntegerAsBinary(int value){
-        return null;
+        return Integer.toBinaryString(value);
     }
 
     public String printIntegerAsOctal(int value){
-        return null;
+        return Integer.toOctalString(value);
     }
 
     public String printIntegerAsHexadecimal(int value){
-        return null;
+        return Integer.toHexString(value);
     }
 
     public static void main(String[] args){
