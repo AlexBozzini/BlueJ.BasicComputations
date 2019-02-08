682e2f0f9ecc71b016d90d9f13bdd762cedd44b6
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-10.local>
2019-02-08 12:00:40 -0500

    First attempt at short calculator

--- ShortCalculator.java
+++ ShortCalculator.java
@@ -1,5 +1,28 @@
  
 
 
+import java.util.Scanner;
 public class ShortCalculator {
+    public static void main (String args[]){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter two numbers (range:0-65535)");
+    
+    int x = in.nextInt();
+    int y = in.nextInt();
+    
+    short xShort = (short) x;
+    short yShort = (short) y;
+    
+    short sum = (short)(xShort + yShort);
+    short difference = (short)(xShort - yShort);
+    short product = (short)(xShort * yShort);
+    short quotient = (short)(xShort / yShort);
+    short remainder = (short)(xShort % yShort);
+    
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+    System.out.println(remainder);
+}
 }
